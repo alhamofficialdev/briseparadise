6d850190664e9d02d0bb109c01cce5c9747a0554
alhamofficial <72916599+alhamofficialdev@users.noreply.github.com>
2023-02-11 01:49:12 +0900

    Add files via upload

--- app/js/app.js
+++ app/js/app.js
@@ -299,7 +299,7 @@
 
     var arlo_tm_animate_text = function() {
         var animateSpan = jQuery(".arlo_tm_animation_text_word");
-        animateSpan.typed({ strings: ["Brise", "Paradise"], loop: true, startDelay: 1e3, backDelay: 2e3 });
+        animateSpan.typed({ strings: ["BRISE", "PARADISE"], loop: true, startDelay: 1e3, backDelay: 2e3 });
     }
 
     var arlo_tm_about_animation = function() {
